0cb0720bedaff1cfe1f72c9e66ca5a6fe3cea04f
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-05-04 23:10:37 -0600

    Add company communication with license service.

--- src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -2,11 +2,14 @@ package com.optimagrowth.license.service;
 
 import com.optimagrowth.license.config.ServiceConfig;
 import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.model.Organization;
 import com.optimagrowth.license.repository.LicenseRepository;
+import com.optimagrowth.license.service.client.OrganizationDiscoveryClient;
+import com.optimagrowth.license.service.client.OrganizationFeignClient;
+import com.optimagrowth.license.service.client.OrganizationRestTemplate;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
-import java.util.Locale;
 import java.util.UUID;
 
 @Service
@@ -14,15 +17,26 @@ public class LicenseService {
   private final MessageSource messages;
   private final LicenseRepository licenseRepository;
   private final ServiceConfig config;
+  private final OrganizationFeignClient organizationFeignClient;
+  private final OrganizationRestTemplate organizationRestClient;
+  private final OrganizationDiscoveryClient organizationDiscoveryClient;
 
   public LicenseService(
-      MessageSource messages, LicenseRepository licenseRepository, ServiceConfig serviceConfig) {
+      MessageSource messages,
+      LicenseRepository licenseRepository,
+      ServiceConfig config,
+      OrganizationFeignClient organizationFeignClient,
+      OrganizationRestTemplate organizationRestClient,
+      OrganizationDiscoveryClient organizationDiscoveryClient) {
     this.messages = messages;
     this.licenseRepository = licenseRepository;
-    this.config = serviceConfig;
+    this.config = config;
+    this.organizationFeignClient = organizationFeignClient;
+    this.organizationRestClient = organizationRestClient;
+    this.organizationDiscoveryClient = organizationDiscoveryClient;
   }
 
-  public License getLicense(String licenseId, String organizationId) {
+  public License getLicense(String licenseId, String organizationId, String clientType) {
     License license =
         this.licenseRepository
             .findByOrganizationIdAndLicenseId(licenseId, organizationId)
@@ -33,7 +47,39 @@ public class LicenseService {
                             messages.getMessage("license.search.error.message", null, null),
                             licenseId,
                             organizationId)));
+    Organization organization = this.retrieveOrganizationInfo(organizationId, clientType);
+    if (organization != null) {
+      license.setOrganizationName(organization.getName());
+      license.setContactName(organization.getContactName());
+      license.setContactEmail(organization.getContactEmail());
+      license.setContactPhone(organization.getContactPhone());
+    }
+
     return license.withComment(this.config.getProperty());
+  }
+
+  private Organization retrieveOrganizationInfo(String organizationId, String clientType) {
+    Organization organization = null;
+
+    switch (clientType) {
+      case "feign":
+        System.out.println("I am using the feign client");
+        organization = this.organizationFeignClient.getOrganization(organizationId);
+        break;
+      case "rest":
+        System.out.println("I am using the rest client");
+        organization = this.organizationRestClient.getOrganization(organizationId);
+        break;
+      case "discovery":
+        System.out.println("I am using the discovery client");
+        organization = this.organizationDiscoveryClient.getOrganization(organizationId);
+        break;
+      default:
+        organization = this.organizationRestClient.getOrganization(organizationId);
+        break;
+    }
+
+    return organization;
   }
 
   public License createLicense(License license) {
