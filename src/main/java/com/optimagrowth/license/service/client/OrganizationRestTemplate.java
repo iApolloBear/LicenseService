5face366d7d0a85551bd291aadd6af702337615f
Aldo Espinosa <aldoespinosaperez1@gmail.com>
2025-05-04 22:59:14 -0600

    Change field injection to constructor injection.

--- src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplate.java
+++ src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplate.java
@@ -1,7 +1,6 @@
 package com.optimagrowth.license.service.client;
 
 import com.optimagrowth.license.model.Organization;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -9,7 +8,11 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class OrganizationRestTemplate {
-  @Autowired RestTemplate restTemplate;
+  private final RestTemplate restTemplate;
+
+  public OrganizationRestTemplate(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
 
   public Organization getOrganization(String organizationId) {
     ResponseEntity<Organization> restExchange =
